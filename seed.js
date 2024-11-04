7add4ce14ad33dcc7ac2e3d0848da185e6fd3a23
BruceWan1021 <2583809090@qq.com>
2024-11-04 18:56:40 +0000

    surround seed Database with loop to make sure it is connected

--- seed.js
+++ seed.js
@@ -4,10 +4,29 @@ const uri = 'mongodb://admin:secret@mongodb:27017';
 const seedData = require('./seeding.json');
 const Movies = require('./model/movies.js');
 
-async function seedDatabase() { 
+async function connectWithRetry() {
+    while (true) {
+        try {
+            await mongoose.connect(uri); // 移除 useNewUrlParser 和 useUnifiedTopology
+            console.log("Successfully connected to MongoDB!");
+            break; // 连接成功，退出循环
+        } catch (error) {
+            console.error("MongoDB connection unsuccessful, retrying after 5 seconds...");
+            await new Promise(resolve => setTimeout(resolve, 5000)); // 等待 5 秒再重试
+        }
+    }
+}
+
+async function seedDatabase() {
     try {
-        await mongoose.connect(uri); 
-        console.log("Successfully connected to MongoDB!");
+        await connectWithRetry(); // 使用重试机制连接 MongoDB
+
+        await Movies.deleteMany({});
+        console.log("Old data is cleared!");
+
+        await Movies.insertMany(seedData);
+        console.log("Database is seeded");
+
     } catch (error) {
         console.error("Something went wrong during seeding database", error);
     } finally {
